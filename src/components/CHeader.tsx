0217a089163a1f384da7c531f40947a5519a2be3
Marta Sušek <m.susek@live.com>
2020-04-23 21:06:06 +0200

    Style header and select.

--- src/components/CHeader.tsx
+++ src/components/CHeader.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
-import {Toolbar} from '@material-ui/core'
+import {Toolbar, makeStyles, createStyles, Theme} from '@material-ui/core'
 import { CSelect } from './CSelect';
-import styled from 'styled-components';
-
-const CHeaderContainer=styled(Toolbar)`
-    background-color: ${({theme}) => theme.colors.primary.background};
-    .MuiFormLabel-root, .MuiInputBase-root{
-       color: ${({theme}) => theme.colors.primary.text};
-       svg, :before{
-       color: ${({theme}) => theme.colors.primary.text};
-
-       }
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    root: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    select:{
+        color: theme.palette.getContrastText(theme.palette.primary.main)
     }
-`
+  }));
 export function CHeader(){
+    const classes = useStyles()
     return(
-        <CHeaderContainer>
+        <Toolbar className={classes.root} >
             <CSelect></CSelect>
-        </CHeaderContainer>
+        </Toolbar>
     )
 }
--- src/components/CSelect.tsx
+++ src/components/CSelect.tsx
@@ -1,11 +1,19 @@
 
 import React from 'react';
-import { FormControl, InputLabel, Select, MenuItem } from '@material-ui/core'
+import { FormControl, InputLabel, Select, MenuItem, makeStyles, createStyles, Theme } from '@material-ui/core'
 import { useSelector, useDispatch } from 'react-redux';
 import { selectAvailableCities, selectSelectedCity } from '../redux/weather/weather.selectors';
 import { selectCity } from '../redux/weather/weather.actions';
 
-export function CSelect() {
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  select: {
+    '& div, & label, & svg': {
+      color: theme.palette.getContrastText(theme.palette.primary.main)
+    }
+  }
+}));
+export function CSelect({ theme }: { theme?: any }) {
+  const classes = useStyles()
   const availableCities = useSelector(selectAvailableCities)
   const selectedCity = useSelector(selectSelectedCity)
   const dispatch = useDispatch()
@@ -16,11 +24,11 @@ export function CSelect() {
   }
 
   return (
-    <FormControl color="primary">
+    <FormControl color="secondary" className={classes.select}  >
       <InputLabel id="select-label">Select city</InputLabel>
       <Select
-        labelId="demo-simple-select-label"
-        id="demo-simple-select"
+        labelId="selectCity"
+        id="select-city"
         value={selectedCity.id}
         onChange={handleChange}
       >
