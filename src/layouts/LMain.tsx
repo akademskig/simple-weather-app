f8339f216f9d9987b3f97b5bc8baf2c9000cf122
Marta Sušek <m.susek@live.com>
2020-04-24 13:22:02 +0200

    Style main layout.

--- src/layouts/LMain.tsx
+++ src/layouts/LMain.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Fragment } from 'react';
 import { CHeader } from '../components/CHeader';
+import { makeStyles, Theme, createStyles } from '@material-ui/core';
+import clouds from '../assets/cloudsWallpaper.jpg'
+const useStyles = makeStyles((theme: Theme) => createStyles({
+    container: {
+        height: `calc(90vh - 64px)`,
+        backgroundImage: ` url(${clouds})`,
+        backgroundColor: '#000',
+        backgroundSize: 'cover',
+        opacity: 0.9,
+        paddingTop: '10vh',
+        [theme.breakpoints.down('xs')]: {
+            height: `calc(90vh - 56px)`,
+            paddingLeft: '1em',
+            paddingRight: '1em'
+        }
+    },
+}));
 
-const LMainContainer = styled.div`
-`
-export function LMain({children}:{children: any}) {
+export function LMain({ children }: { children: any }) {
+
+    const classes = useStyles()
     return (
-        <LMainContainer>
-          <CHeader></CHeader>
-            {children}
-        </LMainContainer>
+        <Fragment>
+            <CHeader></CHeader>
+            <div className={classes.container}>
+                {children}
+            </div>
+        </Fragment>
     )
 }
