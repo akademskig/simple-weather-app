33af69a0609c0f04513e684fd4fbc43ff02b7c61
Marta Sušek <m.susek@live.com>
2020-04-26 19:48:10 +0200

    Change tooltip styles.

--- src/components/CTooltip.tsx
+++ src/components/CTooltip.tsx
@@ -2,8 +2,8 @@ import { withStyles, Theme, Tooltip } from '@material-ui/core';
 
 export const CTooltip = withStyles((theme: Theme) => ({
     tooltip: {
-        backgroundColor: theme.palette.common.white,
-        color: 'rgba(0, 0, 0, 0.87)',
+        backgroundColor: theme.palette.grey[900],
+        color: theme.palette.grey[300],
         boxShadow: theme.shadows[2],
         fontSize: '0.8em',
     },
